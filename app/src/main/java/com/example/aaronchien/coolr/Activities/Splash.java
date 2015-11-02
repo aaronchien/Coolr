c19cfd60cfce9a1228c0b973e03c64c917614965
gnaazr95 <gurnaaz@gmail.com>
2015-11-02 13:33:02 -0800

    Added mediaplayer library for splash.

--- app/src/main/java/com/example/aaronchien/coolr/Activities/Splash.java
+++ app/src/main/java/com/example/aaronchien/coolr/Activities/Splash.java
@@ -2,12 +2,15 @@ package com.example.aaronchien.coolr.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.example.aaronchien.coolr.R;
 
 public class Splash extends Activity {
 	public MediaPlayer mp1;
