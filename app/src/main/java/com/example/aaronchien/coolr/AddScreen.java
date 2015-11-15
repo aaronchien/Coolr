8a779d4539a6b9e8de6de3df21b5e6e569042ac2
gnaazr95 <gurnaaz@gmail.com>
2015-11-15 12:38:07 -0800

    Update AddScreen.java

--- app/src/main/java/com/example/aaronchien/coolr/AddScreen.java
+++ app/src/main/java/com/example/aaronchien/coolr/AddScreen.java
@@ -26,7 +26,9 @@ public class AddScreen extends AppCompatActivity implements View.OnClickListener
     TextView tv;
     Button addButton;
     FoodDBHandler db;
-
+    AutoCompleteTextView addfood; 
+	TextView showcall; 
+ 	public String result = ""; 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +43,70 @@ public class AddScreen extends AppCompatActivity implements View.OnClickListener
         expiration = (DatePicker) findViewById(R.id.expirationDate);
         addButton = (Button) findViewById(R.id.button2);
         addButton.setOnClickListener(this);
+        
+        addfood = (AutoCompleteTextView)findViewById(R.id.fooditem);
+		showcall = (TextView)findViewById(R.id.showcall);
+		addfood.addTextChangedListener(new TextWatcher() {
+			
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				if(s.length() > 2)
+				{
+					URL url = null;
+					String nospace = s.toString().replaceAll("\\s","%20");
+			 		try {
+			 			url = new URL("https://api.nutritionix.com/v1_1/search/" + nospace +"?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=dcfa1c97&appKey=4c4f3685e4270a5e4c1cdef54953ede7");
+			 		} catch (MalformedURLException e) {
+			 			// TODO Auto-generated catch block
+			 			e.printStackTrace();
+			 		}
+			 		   AsyncTask<URL, Integer, String> theResult = new Nutrition().execute(url);
+			 		   String totalString = "";
+			 		try {
+			 			totalString = theResult.get();
+			 		} catch (InterruptedException e) {
+			 			// TODO Auto-generated catch block
+			 			e.printStackTrace();
+			 		} catch (ExecutionException e) {
+			 			// TODO Auto-generated catch block
+			 			e.printStackTrace();
+			 		}
+			 		List<String> nameItems = new ArrayList<String>();
+			 		try {
+						JSONObject autocomplete= new JSONObject(totalString);
+						JSONArray newarray = autocomplete.getJSONArray("hits");
+						for(int i = 0; i < newarray.length(); i++)
+						{
+							JSONObject currentObject = newarray.getJSONObject(i);
+				          	JSONObject innerData = currentObject.getJSONObject("fields");
+				          	String name = innerData.getString("item_name");
+				          	String brand = innerData.getString("brand_name");
+				          	nameItems.add(name + " from " + brand);
+						}
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			 		final String[] strarray = nameItems.toArray(new String[0]);			 		
+			 		String concat = "";
+			 		for(int k = 0; k < strarray.length; k++)
+			 		{
+			 			concat = concat + "," + strarray[k];
+			 		}
+			 		addDropDown(strarray);
+				}
+			}
+			
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
     }
 
     @Override
@@ -54,6 +120,37 @@ public class AddScreen extends AppCompatActivity implements View.OnClickListener
         startActivity(ourIntent);
     }
 
+    public void addDropDown(String[] input)
+	{
+		AutoCompleteTextView curr = (AutoCompleteTextView) findViewById(R.id.fooditem);
+		AddScreen<String> adapter1;
+		adapter1 = new AddScreen<String>(AddScreen.this,
+		            android.R.layout.simple_dropdown_item_1line, input);
+		curr.setAdapter(adapter1);
+		curr.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+					result = parent.getItemAtPosition(position).toString();
+					processResult();
+				}
+			});
+	}
+
+	public void processResult() {
+	    String newvalue = result;
+	    showcall.setText(newvalue);
+	   // try {
+			//Class ourClass = Class.forName("com.example.coolr.MainActivity");
+			//Intent ourIntent = new Intent(Addition.this, ourClass);
+			//ourIntent.putExtra("key", newvalue);
+			//startActivity(ourIntent);
+			//}
+			//catch(ClassNotFoundException e)
+			//{
+			//	e.printStackTrace();
+			//}		    //intent.putExtra("key", newvalue);
+	    //startActivity(intent);
+	}
+	
 }
 
 
